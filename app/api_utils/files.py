061b03332fbae990f674c11559aa516a81464584
erling <nupen.erling@gmail.com>
2026-02-14 23:32:51 +0100

    standardized my read csv function to return a arrow table

--- app/api_utils/files.py
+++ app/api_utils/files.py
@@ -1,6 +1,6 @@
 from fastapi import UploadFile, HTTPException
 import pandas as pd 
-import pyarrow as pd 
+import pyarrow as pa
 import duckdb 
 import logging
 import os 
@@ -32,35 +32,18 @@ class HandleFiles:
         return file
 
     @staticmethod
-    async def read_mb_csv(file_path: str):
+    async def read_csv(file_path: str) -> pa.Table:
         """
-        For reading mobilbanken csv files
+        For reading csv files and return a arrow table
         """
-        
         logger.info(f"Reading Mobilbanken CSV file: {os.path.basename(file_path)}")
 
+        df = pd.read_csv(file_path, delimiter=';',encoding='unicode_escape')
 
-        df = pd.read_csv(transactions_path, delimiter=';',encoding='unicode_escape')
+        logger.info(f"Converting to arrow table")
+
         arrow = pa.Table.from_pandas(df=df)
-
         
-        row = duckdb.sql("""
-            SELECT
-                count(*) as number_of_transactions,
-                MIN(amount) AS min_amount,
-                MAX(amount) AS max_amount,
-                AVG(amount) AS avg_amount
-            FROM arrow
-        """).fetchone()
-
-        number_of_transactions,min_amount, max_amount, avg_amount = row 
-
-        metadata = {"number_of_transactions": number_of_transactions,
-                    "min_amount": min_amount ,
-                    "max_amount": max_amount,
-                    "avg_amount": avg_amount,}
-
-
-        return arrow_cleaned, metadata
+        return arrow
 
 
